ee17e00d3255091401778768e53da3af540a6dbf
ctw00588 <Diana.SA.Silva@ctw.bmwgroup.com>
2019-11-13 09:01:43 +0000

    feat: add example for implementation of the strategy design pattern

---
+++ strategy/src/main/java/strategies/PayStrategy.java
@@ -0,0 +1,11 @@
+package strategies;
+
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+
+    boolean pay(int paymentAmount);
+
+    void collectPaymentDetails();
+}
